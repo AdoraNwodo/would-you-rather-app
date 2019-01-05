22d708d2e4781013c42cbb78346806f89df34b8f
nennenwodo <nennenwodo@gmail.com>
2019-01-05 09:46:44 +0100

    Added Function Components

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
 
-class Login extends Component {
-    render() {
-      return (
-          <h1 className="title">Would you rather</h1>
-      );
-    }
-  }
-  
-  export default Login;
+export default function Header(){
+    return <h1 className="title">Would you rather</h1>;
+}
---
+++ app/src/components/Question.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Question extends Component {
+    render() {
+      return (
+        <div></div>
+      );
+    }
+  }
+  
+  export default Question;
