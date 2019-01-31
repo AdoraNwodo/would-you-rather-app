ddf1615c631a910e0298b14707903a9c6178b1a6
nennenwodo <nennenwodo@gmail.com>
2019-01-31 10:11:00 +0100

    Added props to question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react'
-import Nav from './Nav';
+import Nav from './Nav'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 class Question extends Component {
     render() {
+      const { question } = this.props
+        
+      if(question === null){
+          return <p>This question does not exist</p>
+      }
       return (
         <div className="text-center"> 
           <Nav />
           <br />
+          
           <div className="card center-block">
-            <img src="https://via.placeholder.com/150" className="profile-image"/>
-            <h3>
-                <small>Nenne Nwodo asks:</small>
-            </h3>
+            { this.props.author && 
+            <div>
+              <img src={this.props.author.avatarURL} className="profile-image"/>
+              <h3>
+                  <small>{this.props.author.name} asks:</small>
+              </h3>
+            </div>}
             <p>Would you rather?</p>
+            { this.props.question && 
             <form action="" className="question-form">
-              <input type="radio" name="gender" value="male" /> Let your friend find 100 dollars
+              <input type="radio" name="gender" value="male" /> {this.props.question.optionOne.text}
               <br />
               <span>Or</span>
               <br />
-              <input type="radio" name="gender" value="female" /> Be the one to find 100 dollars
+              <input type="radio" name="gender" value="female" /> {this.props.question.optionTwo.text}
               <br />
-            </form>
+            </form>}
             <button className="addquestion">
                 Submit
             </button>
@@ -30,5 +41,20 @@ class Question extends Component {
       );
     }
   }
+
+  function mapStateToProps({authedUser, questions, users}, props){
+    const { id } = props.match.params;
+
+    return {
+      question: questions
+              ? questions[id]
+              : null,
+      author: users
+              ? users[questions[id].author]
+              : null,  
+    }
+    
+    
+  }
   
-  export default Question;
+  export default connect(mapStateToProps)(Question);
